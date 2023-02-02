8814995130c654d4d755b17fb9580128801e5521
C0722G1-Le-Ta-Ha-Phuong <letahaphuong@gmail.com>
2023-02-02 08:53:16 +0700

    PhuongLTH up foder form,notification

--- C0722G1-Repo-FE/src/app/app-routing.module.ts
+++ C0722G1-Repo-FE/src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'employee', loadChildren: () => import('./employee/employee.module').then(module => module.EmployeeModule)},
   {path: 'post', loadChildren: () => import('./post/post.module').then(module => module.PostModule)},
-  {path: 'customer', loadChildren: () => import('./customer/customer.module').then(module => module.CustomerModule)}
+  {path: 'customer', loadChildren: () => import('./customer/customer.module').then(module => module.CustomerModule)},
+  {path: 'form', loadChildren: () => import('./form/form.module').then(module => module.FormModule)},
+  {path: 'notification' , loadChildren: () => import('./notification/notification.module').then(module => module.NotificationModule)}
 ];
 
 @NgModule({
