d24727ea2e1112601dc3784bf882e515af68ff99
Camvan955 <nguyenvann955@gmail.com>
2023-02-03 16:42:06 +0700

    VanNTC update code customer edit

--- C0722G1-Repo-FE/src/app/customer/customer-edit/customer-edit.component.ts
+++ C0722G1-Repo-FE/src/app/customer/customer-edit/customer-edit.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Customer} from '../../entity/customer/customer';
-import {FormControl, FormGroup} from '@angular/forms';
-import {CustomerService} from '../../service/customer.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {CustomerService} from '../../service/customer/customer.service';
 import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
@@ -18,16 +18,16 @@ export class CustomerEditComponent implements OnInit {
               private activatedRoute: ActivatedRoute) {
     this.editForm = new FormGroup({
       idCustomer: new FormControl(),
-      nameCustomer: new FormControl(),
-      emailCustomer: new FormControl(),
-      addressCustomer: new FormControl(),
-      idCardCustomer: new FormControl(),
+      nameCustomer: new FormControl('', [Validators.minLength(5), Validators.maxLength(50), Validators.required]),
+      emailCustomer: new FormControl('', [Validators.required, Validators.email]),
+      addressCustomer: new FormControl('', [Validators.required]),
+      idCardCustomer: new FormControl('', [Validators.required]),
       codeCustomer: new FormControl(),
-      genderCustomer: new FormControl(),
-      dateOfBirth: new FormControl(),
+      genderCustomer: new FormControl('', [Validators.required]),
+      dateOfBirth: new FormControl('', [Validators.required]),
       approvalCustomer: new FormControl(),
-      phoneCustomer1: new FormControl(),
-      phoneCustomer2: new FormControl(),
+      phoneCustomer1: new FormControl('', [Validators.required, Validators.pattern('(((\\+|)84)|0)(3|5|7|8|9)+([0-9]{8})')]),
+      phoneCustomer2: new FormControl('', [Validators.required, Validators.pattern('(((\\+|)84)|0)(3|5|7|8|9)+([0-9]{8})')]),
       account: new FormControl()
     });
 
@@ -42,6 +42,10 @@ export class CustomerEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * Create by: VanNTC
+   * Date created: 31/01/2023
+   */
   // tslint:disable-next-line:typedef
   getCustomerById(idCustomer: number) {
     this.customerService.findById(idCustomer).subscribe(data => {
@@ -49,6 +53,10 @@ export class CustomerEditComponent implements OnInit {
     });
   }
 
+  /**
+   * Create by: VanNTC
+   * Date created: 31/01/2023
+   */
   // tslint:disable-next-line:typedef
   updateCustomer() {
     const customer = this.editForm.value;
