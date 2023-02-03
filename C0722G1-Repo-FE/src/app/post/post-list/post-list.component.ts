fc8a91c715e389aa0db2fbe5e0de7e7caa14a33e
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-02-03 19:35:29 +0700

    SangNP update code post list screen

--- C0722G1-Repo-FE/src/app/post/post-list/post-list.component.ts
+++ C0722G1-Repo-FE/src/app/post/post-list/post-list.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {LandType} from '../../entity/post/land-type';
+import {PostListService} from './post-list.service';
+import {City} from '../../entity/post/city';
+import {Direction} from '../../entity/post/direction';
 
 @Component({
   selector: 'app-post-list',
@@ -8,14 +11,62 @@ import {LandType} from '../../entity/post/land-type';
 })
 export class PostListComponent implements OnInit {
   landTypeList: LandType[] = [];
+  cityList: City[] = [];
+  directionList: Direction[] = [];
 
-  constructor() {
+  constructor(private postListService: PostListService) {
   }
 
   ngOnInit(): void {
+    this.getLandType();
+    this.getCity();
+    this.getDirection();
   }
 
+  /**
+   * Create by: SangNP
+   * Date created: 03/02/2023
+   * Function: take list land type
+   * @return LandType[]
+   */
   getLandType(): void {
+    this.postListService.getLandType().subscribe(data => {
+        this.landTypeList = data;
+      }, error => {
+      },
+      () => {
+      });
+  }
+  /**
+   * Create by: SangNP
+   * Date created: 03/02/2023
+   * Function: take list city
+   * @return City[]
+   */
+  getCity(): void {
+    this.postListService.getCity().subscribe(data => {
+        this.cityList = data;
+      }, error => {
+      },
+      () => {
+      });
+  }
+  /**
+   * Create by: SangNP
+   * Date created: 03/02/2023
+   * Function: take directionList
+   * @return Direction[]
+   */
+  getDirection(): void {
+    this.postListService.getDirection().subscribe(data => {
+        this.directionList = data;
+      }, error => {
+      },
+      () => {
+      });
+  }
+
+  search(direction: string, city: string, price: string, landType: string, area: string) {
 
   }
 }
