9d1faa84bb6706be6d7dcb007a1ab20858ee3b4e
unknown <you@example.com>
2023-02-03 16:08:06 +0700

    HocHH update resource code Display Customer List but lack entity_customer and service_customer

--- C0722G1-Repo-FE/src/app/customer/customer-list/customer-list.component.ts
+++ C0722G1-Repo-FE/src/app/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Customer} from "../../entity/customer/customer";
+import {CustomerService} from "../../service/customer/customer.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customer-list',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  customer: Customer[] = [];
+  temp: Customer = {};
+  allSearch = '';
+  pageable: any;
+
+  constructor(private customerService: CustomerService,
+              private router: Router) {
+    this.getAllCustomerListComponent(this.pageable);
+
+  }
+
+  private getAllCustomerListComponent(pageable: any): void {
+    this.allSearch = '';
+    this.customerService.getAllCustomerPaging(this.allSearch, pageable).subscribe(data => {
+      console.log(data);
+      // @ts-ignore
+      this.customer = data.content;
+      // @ts-ignore
+      this.pageable = data.pageable;
+      console.log(this.customer);
+      console.log(this.pageable);
+    }, error => {
+    }, () => {
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  reload(): void {
+    console.log(this.pageable);
+    this.getAllCustomerListComponent(this.pageable);
+  }
 }
