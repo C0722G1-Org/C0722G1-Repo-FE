f0d9906135f52a91f4b3d4a52fe0e32dfcba95e8
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-02-06 09:44:16 +0700

    SangNP update code

--- C0722G1-Repo-FE/src/app/post/post-list/post-list.component.ts
+++ C0722G1-Repo-FE/src/app/post/post-list/post-list.component.ts
@@ -3,6 +3,11 @@ import {LandType} from '../../entity/post/land-type';
 import {PostListService} from './post-list.service';
 import {City} from '../../entity/post/city';
 import {Direction} from '../../entity/post/direction';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {PostListHome} from '../../entity/post/post-list-home';
+import {Image} from '../../entity/post/image';
+import {forEachComment} from 'tslint';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-post-list',
@@ -13,14 +18,30 @@ export class PostListComponent implements OnInit {
   landTypeList: LandType[] = [];
   cityList: City[] = [];
   directionList: Direction[] = [];
+  formSearch: FormGroup;
+  page = 0;
+  postList: PostListHome[] = [];
+  postListTemp: PostListHome[] = [];
+  totalPage = 0;
+  mess = '';
+  imageList: Image[] = [];
+  image = '';
 
-  constructor(private postListService: PostListService) {
+  constructor(private postListService: PostListService, private fb: FormBuilder) {
+    this.formSearch = this.fb.group({
+      area: [''],
+      price: [''],
+      landType: [''],
+      city: [''],
+      direction: ['']
+    });
   }
 
   ngOnInit(): void {
     this.getLandType();
     this.getCity();
     this.getDirection();
+    this.getPostPage();
   }
 
   /**
@@ -37,6 +58,7 @@ export class PostListComponent implements OnInit {
       () => {
       });
   }
+
   /**
    * Create by: SangNP
    * Date created: 03/02/2023
@@ -51,6 +73,7 @@ export class PostListComponent implements OnInit {
       () => {
       });
   }
+
   /**
    * Create by: SangNP
    * Date created: 03/02/2023
@@ -66,7 +89,107 @@ export class PostListComponent implements OnInit {
       });
   }
 
-  search(direction: string, city: string, price: string, landType: string, area: string) {
+  /**
+   * Create by: SangNP
+   * Date created: 03/02/2023
+   * Function: take image List by post id
+   * @return Image[]
+   */
+  getImageByIdPost(list: PostListHome[]): void {
+    for (let i = 0; i < list.length; i++) {
+      this.postListService.getImageByIdPost(list[i].idPost).subscribe(data => {
+          this.imageList = data;
+          if (this.imageList.length > 0) {
+            list[i].imageURL = this.imageList[0].url;
+          }
+        }, error => {
+        },
+        () => {
+        }
+      );
+    }
+  }
 
+  /**
+   * Create by: SangNP
+   * Date created: 04/02/2023
+   * Function: take post list
+   * @return void
+   */
+  getPostPage(): void {
+    this.postListService.getPostPage(
+      this.formSearch.controls.area.value,
+      this.formSearch.controls.price.value,
+      this.formSearch.controls.landType.value,
+      this.formSearch.controls.direction.value,
+      this.formSearch.controls.city.value, this.page).subscribe(data => {
+        this.mess = '';
+        this.postListTemp = data.content;
+        this.totalPage = data.totalPages;
+        this.page = data.pageable.pageNumber;
+        if (this.postListTemp.length > 0) {
+          this.getImageByIdPost(this.postListTemp);
+        }
+        this.postList = this.postList.concat(this.postListTemp);
+      }, error => {
+        this.mess = 'Không có dữ liệu';
+      },
+      () => {
+      });
+  }
+
+  /**
+   * Create by: SangNP
+   * Date created: 04/02/2023
+   * Function: to show more post
+   * @return void
+   */
+  showMore(): void {
+    if (this.page < this.totalPage - 1) {
+      this.page = this.page + 1;
+      this.getPostPage();
+    }
+  }
+
+  /**
+   * Create by: SangNP
+   * Date created: 04/02/2023
+   * Function: to show less post
+   * @return void
+   */
+  showLess(): void {
+    this.postList = [];
+    this.page = 0;
+    this.getPostPage();
+  }
+
+  /**
+   * Create by: SangNP
+   * Date created: 04/02/2023
+   * Function: take post list
+   * @return void
+   */
+  search(): void {
+    this.postList = [];
+    this.getPostPage();
+  }
+  /**
+   * Create by: SangNP
+   * Date created: 04/02/2023
+   * Function: reset search
+   * @return void
+   */
+  resetSearch(): void {
+    this.formSearch.setValue({
+      area: [''],
+      price: [''],
+      landType: [''],
+      city: [''],
+      direction: ['']
+    });
+    this.postList = [];
+    console.log(this.postList);
+    this.getPostPage();
+    console.log(this.postList);
   }
 }
