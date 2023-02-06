c4b21fecaab07a684c9dc54108ee33efbbea05bf
trinhminhduc9x <duc1997th@gmail.com>
2023-02-06 10:02:04 +0700

    add

--- C0722G1-Repo-FE/src/app/service/customer.service.ts
+++ C0722G1-Repo-FE/src/app/service/customer.service.ts
@@ -10,17 +10,31 @@ import {environment} from '../../environments/environment';
 })
 export class CustomerService {
 
+  /**
+   * creator: Trịnh Minh Đức
+   * date:31/01/2023
+   * method of using save customer
+   */
   private urlCustomer = 'http://localhost:8080/api/public/signup';
   private  urlListMaileCustomer = 'http://localhost:8080/api/public/ListMailCustomerAnhNameAccount' ;
 
   constructor(private httpClient: HttpClient) { }
 
-
+  /**
+   * creator: Trịnh Minh Đức
+   * date:31/01/2023
+   * method of using save customer
+   */
   // tslint:disable-next-line:typedef
   saveCustomer(customer: Customer | undefined) {
     console.log(customer);
     return this.httpClient.post<Customer>(this.urlCustomer, customer);
   }
+  /**
+   * creator: Trịnh Minh Đức
+   * date:31/01/2023
+   * method of using save customer
+   */
   findListMailCustomerr(): Observable<Customer[]> {
     return this.httpClient.get<Customer[]>(this.urlListMaileCustomer);
   }
