3ef2bf4bd6b4ca8db69dd1afabf28885237d78b1
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-02-09 13:17:11 +0700

    Update app.module.ts

--- C0722G1-Repo-FE/src/app/app.module.ts
+++ C0722G1-Repo-FE/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {HomeModule} from './home/home.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
 import {HttpClientModule} from '@angular/common/http';
-import {NgxPaginationModule} from "ngx-pagination";
 
 @NgModule({
   declarations: [
@@ -19,7 +18,6 @@ import {NgxPaginationModule} from "ngx-pagination";
     HttpClientModule,
     HomeModule,
     NgxPaginationModule,
-    BrowserAnimationsModule,
     ToastrModule.forRoot()
   ],
   providers: [],
